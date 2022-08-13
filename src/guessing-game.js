050fc4b93f25870f9856ee17d24903344e881fee
Oksana <5oks5@mail.com>
2022-08-13 23:47:34 +0400

    solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+  this.left = min;
+  this.right = max;   
     }
 
     guess() {
-
+  this.middle = Math.round((this.right - this.left)/2) + this.left;
+      return this.middle;
+      
     }
 
     lower() {
-
+        this.right = this.guess();
+        return this.right
     }
 
     greater() {
-
+this.left = this.guess();
+    return this.left
     }
 }
 
